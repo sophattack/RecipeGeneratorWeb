87625b3f0f7447ff5c10cef83127657a69f7cbf5
xuxihui <juliaxxh.xu@mail.utoronto.ca>
2018-07-11 14:11:07 -0400

    make little changes to models

--- RecipeGeneratorWeb/AutoGener/models.py
+++ RecipeGeneratorWeb/AutoGener/models.py
@@ -7,7 +7,8 @@ class CanDo(models.Model):
         ('荤', '荤'),
         ('素', '素'),
         ('both', 'both'),
-        ('暂不选择', '暂不选择')
+        ('暂不选择', '暂不选择'),
+        ('主食', '主食')
     )
     name = models.CharField(max_length=100)
     type = models.CharField(max_length=5, choices=TYPE_CHOICES, default='暂不选择')
@@ -19,8 +20,7 @@ class CanDo(models.Model):
 class CanGet(models.Model):
     """ the ingredients user can get"""
     name = models.CharField(max_length=100)
-    price = models.FloatField()
-    cal = models.FloatField()
+    cal = models.FloatField(help_text="千卡/100g")
 
     def __str__(self):
         return self.name
