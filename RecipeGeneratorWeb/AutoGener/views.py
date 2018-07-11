5900bcff04819749f5a41d5907ca986b6a8ff971
xuxihui <juliaxxh.xu@mail.utoronto.ca>
2018-07-11 14:53:42 -0400

    can use templates now

--- RecipeGeneratorWeb/AutoGener/views.py
+++ RecipeGeneratorWeb/AutoGener/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse('yep')
+    return render(request, 'AutoGener/index.html')
