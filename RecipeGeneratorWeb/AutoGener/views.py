0dc69dd23f853d2972a576b24a378a9309df43d3
xuxihui <juliaxxh.xu@mail.utoronto.ca>
2018-07-11 15:22:09 -0400

    add base.html

--- RecipeGeneratorWeb/AutoGener/views.py
+++ RecipeGeneratorWeb/AutoGener/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 def index(request):
-    return render(request, 'AutoGener/index.html')
+    return render(request, 'AutoGener/base.html')
