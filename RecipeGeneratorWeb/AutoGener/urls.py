60373c355fcd8513c238ad3e0e6af89c3d782c49
xuxihui <juliaxxh.xu@mail.utoronto.ca>
2018-07-12 11:20:20 -0400

    little progress on home page

--- RecipeGeneratorWeb/AutoGener/urls.py
+++ RecipeGeneratorWeb/AutoGener/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from . import views
 
+app_name = 'AutoGener'
 urlpatterns = [
-    path('', views.index)
+    path('', views.index, name='index')
 ]
--- RecipeGeneratorWeb/AutoGener/views.py
+++ RecipeGeneratorWeb/AutoGener/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 def index(request):
-    return render(request, 'AutoGener/base.html')
+    return render(request, 'AutoGener/home.html')
