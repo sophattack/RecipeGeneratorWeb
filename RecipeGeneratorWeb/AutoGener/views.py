aa91ec7968a0ed07fa1427edbd168174a9b56104
xuxihui <juliaxxh.xu@mail.utoronto.ca>
2018-07-19 12:27:01 -0400

    functional views.get_dish

--- RecipeGeneratorWeb/AutoGener/views.py
+++ RecipeGeneratorWeb/AutoGener/views.py
@@ -2,41 +2,64 @@ from django.shortcuts import render, redirect
 from .forms import DishForm, IngredientForm
 from .models import CanDo, CanGet
 import random
+from django.core.exceptions import ObjectDoesNotExist
+from django.shortcuts import get_object_or_404
+
 # Create your views here.
 def index(request):
     return render(request, 'AutoGener/home.html')
 
 
+
 def get_dish(request):
     form = DishForm()
     canDolist = CanDo.objects.all()
-    duplicate = False
+    message = ''
     if request.method == 'POST':
         form = DishForm(request.POST)
         # check whether it's valid:
         if form.is_valid():
-            # check duplicate
+            # check notice
             new_dish_name = request.POST.get('name')
-            dish = CanDo.objects.filter(name=new_dish_name)
-            if dish.exists():
-                duplicate = True
-            else:
-                new_dish = form.save(commit=False)
-                new_dish.save()
-                dish_ingredient_str = request.POST.get('dish_ingredient')
-                dish_ingredient_list = dish_ingredient_str.split(',')
-                for ingredient in dish_ingredient_list:
-                    ingre = CanGet.objects.get(name=ingredient)
-                    new_dish.ingre.add(ingre)
+            dish_ingredient_str = request.POST.get('dish_ingredient')
+            dish_ingredient_list = dish_ingredient_str.split('，')
+            try:
+                dish = CanDo.objects.get(name=new_dish_name)
+                # 菜已经存在, 但没有再次输入食材
+                if not dish_ingredient_str:
+                    message = '你已经有这道菜了,请重新输入'
+                    context = {'canDolist': canDolist, 'form': form, 'message': message}
+                    return render(request, 'AutoGener/dishform.html', context)
+                # 菜已经存在，再次输入了食材，删除原食材重建
+                else:
+                    dish.delete()
+                    raise ObjectDoesNotExist
 
-    context = {'canDolist': canDolist, 'form': form, 'duplicate': duplicate}
+            # 菜不存在
+            except ObjectDoesNotExist:
+                # 新建菜
+                dish = form.save(commit=False)
+                dish.save()
+                message = '成功添加'
+            if dish_ingredient_str:
+                for ingredient in dish_ingredient_list:
+                    try:
+                        ingre = CanGet.objects.get(name=ingredient)
+                        dish.ingre.add(ingre)
+                    except ObjectDoesNotExist:
+                        message = "请先添加%s, 再回来添加" % ingredient
+            # 菜存在，但是没有食材
+            else:
+                message = '请输入食材'
+
+    context = {'canDolist': canDolist, 'form': form, 'message': message}
     return render(request, 'AutoGener/dishform.html', context)
 
 
 def get_ingredient(request):
     form = IngredientForm()
     ingredientlist = CanGet.objects.all()
-    duplicate = False
+    duplicate = ''
     if request.method == 'POST':
         form = IngredientForm(request.POST)
         # check whether it's valid:
@@ -45,10 +68,11 @@ def get_ingredient(request):
             new_inge_name = request.POST.get('name')
             dish = CanGet.objects.filter(name=new_inge_name)
             if dish.exists():
-                duplicate = True
+                duplicate = '已有该食材，请重新输入'
             else:
                 new_inge = form.save(commit=False)
                 new_inge.save()
+                duplicate = '添加成功'
 
 
     context = {'ingredientlist': ingredientlist, 'form': form, 'duplicate': duplicate}
