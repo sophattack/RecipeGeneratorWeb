a101cb30638a7287bb0b11615f20d06927acdd80
xuxihui <juliaxxh.xu@mail.utoronto.ca>
2018-07-31 15:51:09 -0400

    add static root

--- RecipeGeneratorWeb/RecipeGeneratorWeb/settings.py
+++ RecipeGeneratorWeb/RecipeGeneratorWeb/settings.py
@@ -120,3 +120,5 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [ os.path.join(BASE_DIR, "statics"),]
+
+STATIC_ROOT = "/home/xihuixu/RecipeGeneratorWeb/RecipeGeneratorWeb/AutoGener/static"
